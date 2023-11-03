e53c4d971c9861eba6370dd8fa87dca8a871dbbb
Getulio Augusto Ferreira de Castro Junior <augustoge71@gmail.com>
2023-11-03 17:49:50 -0300

    Acrescentado estilo com CSS e protegido acesso pela URL, necessita implementação.

--- Login.aspx.cs
+++ Login.aspx.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
+using System.Web.Security;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
@@ -35,10 +36,8 @@ namespace AgendaWebApp
             SqlDataReader reader = cmd.ExecuteReader();
             if (reader.HasRows)
             {
-                //cookie
-                HttpCookie login = new HttpCookie("login", txbEmail.Text);
-                Response.Cookies.Add(login);
-                // entra na página
+                FormsAuthentication.SetAuthCookie();
+                FormsAuthentication.SignOut();
                 Response.Redirect("~/Index.aspx");
 
             }
